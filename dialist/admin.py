5e80d67b5e41b248bd9ebeb59bfa68b2ce29d42a
Chester Jungseok Roh <chester.roh@gmail.com>
2015-09-07 20:26:11 +0900

    admin page modified

--- dialist/admin.py
+++ dialist/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
 from .models import Diamond
 
-admin.site.register(Diamond)
+class DiamondAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Input Date', { 'fields' : ['input_date'], 'classes' : ['collapse'] }),
+        (None, { 'fields' : ['cert_no'] }),
+        (None, { 'fields' : ['carat'] }),
+        (None, { 'fields' : ['clarity'] }),
+        (None, { 'fields' : ['cut'] }),
+        (None, { 'fields' : ['pol'] }),
+        (None, { 'fields' : ['sym'] }),
+        (None, { 'fields' : ['flo'] }),
+        (None, { 'fields' : ['rapa_price'] }),
+        (None, { 'fields' : ['discount_rate'] }),
+        (None, { 'fields' : ['comment'] }),
+        ]
+    list_display = ( 'cert_no', 'carat' , 'clarity', 'cut', 'pol', 'sym' , 'flo', 'rapa_price' , 'discount_rate' )
+    list_filter = ['carat']
+    search_fields = [ 'carat' ]
+               
+admin.site.register(Diamond,DiamondAdmin)
