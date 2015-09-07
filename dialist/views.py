24b8ea9bc9fdda9943e5edec0b982ef50188826f
Chester Jungseok Roh <chester.roh@gmail.com>
2015-09-07 21:27:26 +0900

    view in progress

--- dialist/views.py
+++ dialist/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("This would be the main dia list view page")
+
+def detail(request,diamond_id):
+    response = "you're looking at diamond %s."
+    return HttpResponse( response % diamond_id )
+
+def results(request,diamond_id):
+    response = "You're look the results of diamond %s."
+    return HttpResponse( response % diamond_id )
+
+
--- sjdiamond/urls.py
+++ sjdiamond/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^dialist/', include('dialist.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
